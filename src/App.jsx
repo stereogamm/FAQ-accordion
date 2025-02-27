c01653ce24500db2fb6e4105de55f984c9e72597
Marina Sysoliatina <stereogam@yandex.ru>
2025-02-27 22:10:29 +0100

    add styles, created card and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <h1>FAQ</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          lerning REACT project
-        </p>
-      </div>
-    </>
+      <main className="main">
+       <Card />
+      </main>
+   
   )
 }
 
---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function Card() {
+    return (
+        <>
+         <section className='card'>
+            <h1>FAQs</h1>
+         </section>
+        </>
+    )
+}
+
+export default Card;
