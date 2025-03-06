287c05d5454f2241633f2be5ed7540676e509c07
Marina Sysoliatina <stereogam@yandex.ru>
2025-03-06 14:53:10 +0100

    added open/close logic

---
+++ src/Components/data.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        header: 'Is coding hard?',
+        text: 'Coding is not easy. Sometimes, you spend hours fixing one small mistake.',
+    },
+    {
+        header: 'Does debugging take time?',
+        text: 'Finding errors in code can be difficult. You must be patient and check every detail.',
+    },
+    {
+        header: 'Do I need to learn all the time?',
+        text: 'New technologies come all the time. A good developer always learns new things.',
+    },
+    {
+        header: 'Is logic important?',
+        text: 'Coding is like solving a puzzle. You need to think step by step to make things work.',
+    },
+];
+
+export default data;
