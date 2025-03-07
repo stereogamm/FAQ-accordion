8b3b82f52c446556c7a6d2840e234fdf90e47f38
Marina Sysoliatina <stereogam@yandex.ru>
2025-03-07 17:32:51 +0100

    fixed tab focus

--- src/Components/ListComponent.jsx
+++ src/Components/ListComponent.jsx
@@ -5,25 +5,39 @@ import AccordionButton from "./AccordionButton";
 import data from "./data";
 
 function ListComponent({ openIndex, onClick }) {
- return (
+  
+  return (
     <>
-    {data.map((item, index) => (
-      <div key={index} className="list_item">
-        <div className="list_wrapper">
-          <h2>{item.header}</h2>
-          <AccordionButton 
-            isOpen={openIndex === index}
-            onClick={() => onClick(index)}
-          />
-        </div>
-        <p className={openIndex === index ? "visibleItem" : "hiddenItem"}>
-          {item.text}
-        </p>
-        <hr />
-      </div>
-    ))}
-  </>
- )
+      {data.map((item, index) => {
+        const isOpen = openIndex === index;
+        const headerId = `accordion-header-${index}`;
+        const contentId = `accordion-content-${index}`;
+
+        return (
+          <div key={index} className="list_item">
+            <div className="list_wrapper">
+              <h2 id={headerId} tabIndex="0">{item.header}</h2>
+              <AccordionButton
+                isOpen={openIndex === index}
+                onClick={() => onClick(index)}
+                aria-expanded={isOpen}
+                aria-controls={contentId}
+              />
+            </div>
+            <p
+              role="region"
+              aria-labelledby={headerId}
+              id={contentId}
+              className={openIndex === index ? "visibleItem" : "hiddenItem"}
+            >
+              {item.text}
+            </p>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default ListComponent;
